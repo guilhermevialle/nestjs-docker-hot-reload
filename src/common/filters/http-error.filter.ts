540d2b8b28801012915414e45e09c442a46653cc
Guilherme Vialle <vialleguilherme@gmail.com>
2025-11-23 19:58:08 -0300

    fix: fix error filter

--- src/common/filters/http-error.filter.ts
+++ src/common/filters/http-error.filter.ts
@@ -1,4 +1,4 @@
-import { ArgumentsHost, Catch } from '@nestjs/common';
+import { ArgumentsHost, BadRequestException, Catch } from '@nestjs/common';
 import { Response } from 'express';
 import { ApplicationError } from 'src/application/errors/application.error';
 
@@ -14,6 +14,15 @@ export class HttpErrorFilter {
         statusCode: exception.statusCode,
         errorCode: exception.errorCode,
         timestamp: exception.occurredAt,
+      });
+    }
+
+    if (exception instanceof BadRequestException) {
+      const res: any = exception.getResponse();
+      return response.status(400).json({
+        statusCode: 400,
+        errorCode: 'ERR_BAD_REQUEST',
+        errors: res.message,
       });
     }
 
--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,13 @@
-import { MailerService } from '@nestjs-modules/mailer';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { HttpErrorFilter } from './common/filters/http-error.filter';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  const mailer = app.get(MailerService);
-  try {
-    await mailer.sendMail({
-      to: 'test@example.com',
-      subject: 'TEST',
-      html: '<h1>OK</h1>',
-    });
-    console.log('ENVIADO');
-  } catch (e) {
-    console.log('ERRO SMTP >>>');
-    console.log(e);
-  }
-
   app.useGlobalFilters(new HttpErrorFilter());
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
 
   await app.listen(process.env.PORT ?? 3000);
 }
