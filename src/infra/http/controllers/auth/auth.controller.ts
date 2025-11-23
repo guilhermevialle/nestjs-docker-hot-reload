03731d99e2b47d4eb2c745f48b53fefb99f0c4ce
Guilherme Vialle <vialleguilherme@gmail.com>
2025-11-23 19:54:33 -0300

    fix: fix httpCode for auth controllers

--- src/infra/http/controllers/auth/auth.controller.ts
+++ src/infra/http/controllers/auth/auth.controller.ts
@@ -1,10 +1,13 @@
 import {
   Body,
   Controller,
+  HttpCode,
   Post,
+  Req,
   UsePipes,
   ValidationPipe,
 } from '@nestjs/common';
+import { type Request } from 'express';
 import { AuthService } from 'src/application/services/auth/auth.service';
 import { AuthenticateUserDto } from 'src/application/services/auth/dtos/authenticate-user.dto';
 import { RegisterUserDto } from 'src/application/services/auth/dtos/register-user.dto';
@@ -14,12 +17,14 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('login')
+  @HttpCode(200)
   @UsePipes(new ValidationPipe())
-  async authenticate(@Body() dto: AuthenticateUserDto) {
+  async authenticate(@Body() dto: AuthenticateUserDto, @Req() req: Request) {
     return await this.authService.authenticate(dto);
   }
 
   @Post('register')
+  @HttpCode(201)
   @UsePipes(new ValidationPipe())
   async register(@Body() dto: RegisterUserDto) {
     return await this.authService.register(dto);
